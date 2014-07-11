148818371b860ac4797aad765ad07b741331198e
dimang12 <dimang12@gmail.com>
2014-07-11 11:38:19 +0700

    commit change

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,12 +2,12 @@
 return array(
 		'db' => array(
 				'driver'         => 'Pdo',
-				'dsn'            => 'mysql:dbname=yourjob;host=localhost',
+				'dsn'            => 'mysql:dbname=yourjob;host=166.62.8.49',
 				'driver_options' => array(
 						PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
 				),
-				'username' => 'root',
-				'password' => ''
+				'username' => 'yourjob',
+				'password' => 'Jan121984!'
 		),
 		'service_manager' => array(
 				'factories' => array(
