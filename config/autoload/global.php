9ba60b481a9a066b47f585debd13777393d49932
PONNIMOL <nimolsetec@gmail.com>
2015-01-11 10:59:12 +0700

    update view resume

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,7 +2,7 @@
 return array(
 		'db' => array(
 				'driver'         => 'Pdo',
-				'dsn'            => 'mysql:dbname=ourjob;host=localhost',
+				'dsn'            => 'mysql:dbname=yourjob;host=localhost',
 				'driver_options' => array(
 						PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES \'UTF8\''
 				),
